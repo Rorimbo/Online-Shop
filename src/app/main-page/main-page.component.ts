cb51a6cd21a2a63121c95e594654eeb30b42eace
Rori <andreyanushkin@gmail.com>
2023-04-12 15:31:08 +0300

    NewsDialog

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { NewsDialogComponent } from '../news-dialog/news-dialog.component';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
-  
+  constructor(public dialog: MatDialog) {}
+
+  newsDialog() {
+    const dialogRef = this.dialog.open(NewsDialogComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
